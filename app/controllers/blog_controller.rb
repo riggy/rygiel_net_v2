9cc52b56ec7ebffd80764f902e49d11e87459c7b
Krzysztof Rygielski <krzysztof@rygiel.net>
2026-03-20 14:43:26 +0100

    Rubocop complains...

--- app/controllers/blog_controller.rb
+++ app/controllers/blog_controller.rb
@@ -5,7 +5,7 @@ class BlogController < ApplicationController
     @page = (params[:page] || 1).to_i
     @total = Post.published.count
     @total_pages = (@total / PER_PAGE.to_f).ceil
-    @posts = Post.published.limit(PER_PAGE).offset((@page - 1) * PER_PAGE).map {|post| PostPresenter.new(post)}
+    @posts = Post.published.limit(PER_PAGE).offset((@page - 1) * PER_PAGE).map { |post| PostPresenter.new(post) }
   end
 
   def show
